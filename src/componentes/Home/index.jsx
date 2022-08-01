6c83a5cc7a31497d8b976db0f88c285bc3abaef8
rodrigoorocha <rodrigotherock@hotmail.com>
2022-08-01 15:35:54 -0300

    modal

--- src/componentes/Home/index.jsx
+++ src/componentes/Home/index.jsx
@@ -11,67 +11,6 @@ import api from '../../Service/api';
 export function Home(){
 
 
-    
-//   //Aconst [pesquisa, setPesquisa] = useState('')
-//   const [allPokemons, setAllPokemons] = useState([])
-//   // const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
-
-//   //popular 
-//   const pesquisaOnChange = (event) => {
-//     if (event.target.value === '') {
-
-//       getAllPokemons().then(res => setAllPokemons(() => res))
-//       //flitrar
-//     } else {
-//       getAllPokemons().then(res => setAllPokemons(() => res.filter((p) => p.nome.toUpperCase().includes((event.target.value.toUpperCase())))))
-
-//     }
-
-//   }
-
-//   //pegar pokemons da api
-//   const getAllPokemons = async () => {
-//     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50").then(res => res.json())
-//     const data = response.results
-//     console.log(data)
-
-//     let pokemons = await Promise.all(
-//       //percorrer cada elemento do obj do pokemon
-//       data.map(async (p) => {
-//         const newPokemon = await getStatusPokemon(p.url).then(res => res)
-//         return newPokemon
-//       }))
-
-//     return pokemons
-
-//   }
-
-//   // pegar os dados necessarios e trasformar em obj
-//   const getStatusPokemon = async (urlPokemon) => {
-
-//     const response = await fetch(urlPokemon).then(res => res.json())
-//     const data = response
-//     const pokemonId = data.id
-
-//     const pokemon = {
-//       id: pokemonId,
-//       nome: data.forms[0].name,
-//       image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
-//     }
-//     return pokemon
-
-//   }
-
-
-  // chamar a funçao principal para popular a lista de pokemon
-//   useEffect(() => {
-
-//     getAllPokemons()
-//       .then(res => setAllPokemons(() => res))
-
-//   }, [])
-
-
 const [todosPokemons, setTodosPokemons] = useState([]);
     
 useEffect (()=>{
@@ -97,8 +36,6 @@ itemP.name.toLocaleLowerCase().includes(buscaPokemons.toLocaleLowerCase())
 );
 
 
-
-
 return(
     <div className="App">
 
@@ -108,7 +45,6 @@ return(
       </h1>
       <div className="container display-flex align-center">
         <div className='container-buscador'>
-          {/* para q serve o value */}
           <input className='campo-buscador' onChange={(evento) => setBuscaPokemons(evento.target.value)} value={buscaPokemons} type="search" placeholder='Digite aqui a sua busca...' />
           <button className='btn-buscador' type="submit" />
         </div>
--- src/componentes/itemPokemon.js
+++ src/componentes/itemPokemon.js
@@ -1,28 +1,91 @@
 import '../style/home.css';
+import { useEffect, useState } from 'react';
+//modal
+import Modal from 'react-modal';
+
+// inicio modal
+const customStyles = {
+    content: {
+        top: '33%',
+        left: '86%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+
+//  fim modal
 
 function ItemPokemon({ pokemon }) {
     const id = pokemon.url;
     const URL = id.split("/");
-    const UrlID = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${URL[6]}.png`; 
+    const UrlID = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${URL[6]}.png`;
+
+    // inicio modal
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+    // fim do modal
 
     return (
+        <>
+            <li className='pokemon' key={pokemon.id}>
 
-        <li className='pokemon' key={pokemon.id}>
-            
 
-            <div className='info-pokemon'>
-                <div className='foto-pokemon'>
-                    <img src={UrlID} alt="" />
-                </div>
-                <hr/>
+                <div className='info-pokemon'>
+                    <div className='foto-pokemon'>
+                        <img src={UrlID} alt="" />
+                    </div>
+                    <hr />
                     <h3 className='nome-pokemon'>{pokemon.name}</h3>
-            </div>           
-            <div className='actions'>
-                <button className='btn-excluir' type="button">Excluir</button>
-                <button className='btn-editar'type="button">Editar</button>
+                </div>
+                <div className='actions'>
+                    <button className='btn-excluir' type="button">Excluir</button>
+                    <button className='btn-editar' onClick={openModal} type="button">Editar</button>
+                </div>
+            </li>
+
+        {/* //inico html modal */}
+            <div>
+                
+                <Modal
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+                    <button onClick={closeModal}>close</button>
+                    <div>
+                        <img src={UrlID} alt="" />
+                        <h3 className='nome-pokemon'>{pokemon.name}</h3>
+                    </div>
+                    border
+
+                </Modal>
             </div>
-        </li>
+        </>
+        //fim html modal
     );
 }
 
 export default ItemPokemon;
+
